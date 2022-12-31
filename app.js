436f5eecb06683a90fc8a194903cd8a5ee64bc3e
Daniel Lugo <daniellugofreelance@gmail.com>
2022-12-31 16:44:26 -0300

    added Subs router + controller + model

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 require('dotenv').config()
 require('./config/database')
 
-
-
-
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -16,6 +13,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+var subs = require('./routes/subsRoutes');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,6 +29,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+//myRoutes
+app.use('/newsletter', subs);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
